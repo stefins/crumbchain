86b4313bd1e76f102b5552244c64b56294b067c7
Stef <sstefin@bk.ru>
2020-09-06 23:09:01 -0500

    :zap: Improved performance

--- crumb/crumber.go
+++ crumb/crumber.go
@@ -3,31 +3,32 @@ package crumb
 import (
 	"bufio"
 	"crypto/sha256"
-	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
-	"strings"
+	"encoding/base64"
+	_ "strings"
 	"io"
-	"github.com/cheggaaa/pb/v3"
-	proto "github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"
 )
 
 func Crumber(name string,size int) {
 	//To chuck the file into 1 MB
 	chuck := 1024 * 1024 * size
-	var content strings.Builder
-	cmb := []*Crumb{}
+	//var content strings.Builder
 	f, err := os.Open("./" + name)
 	if err != nil {
 		fmt.Println("File Not Found!")
 		return
 	}
+	os.Mkdir(name+"-crumb", 0755)
 	reader := bufio.NewReader(f)
-	buf := make([]byte,256)
-  for {
+	buf := make([]byte,chuck)
+	var fuck int
+	var prev_crum Crumb
+  for i := 0; ; i++ {
         n, err := reader.Read(buf)
         if err != nil {
            if err != io.EOF {
@@ -35,9 +36,32 @@ func Crumber(name string,size int) {
            }
            break
         }
+				fuck++
+				fmt.Println(fuck)
         //fmt.Print(string(buf[0:n]))
-        content.WriteString(string(buf[0:n]))
+        //content.WriteString(string(buf[0:n]))
+				loc := new(Crumb)
+				if i==0 {
+					loc.PrevHash = []byte("0000000")
+				}else{
+					loc.PrevHash = prev_crum.Hash
+				}
+				h := sha256.New()
+				h.Write([]byte(buf[0:n]))
+				h.Write([]byte(loc.PrevHash))
+				loc.Index = int64(i)
+				loc.Name = name
+				loc.Hash = h.Sum(nil)
+				loc.Content = base64.StdEncoding.EncodeToString([]byte(buf[0:n]))
+				data := &Crumb{Index:loc.Index, Name:loc.Name, Hash:loc.Hash, PrevHash:loc.PrevHash, Content:loc.Content}
+				b,err := proto.Marshal(data)
+				if err != nil {
+					log.Fatal(err)
+				}
+				err = ioutil.WriteFile(name+"-crumb/"+name+strconv.Itoa(i)+".cbc", b, 0644)
+				prev_crum = *loc
   }
+/*
 	fmt.Println("Encoding to base64....")
 	encoded := base64.StdEncoding.EncodeToString([]byte(content.String()))
 
@@ -96,4 +120,5 @@ func Crumber(name string,size int) {
 		bar.Increment()
 	}
 	bar.Finish()
+*/
 }
